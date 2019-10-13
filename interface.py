91cf985cdba02449f69bf54e50ab63ee96360c62
Jack Wu <JackWu@gmx.com>
2019-10-12 20:14:17 -0400

    extract_data function before testing

--- interface.py
+++ interface.py
@@ -28,46 +28,7 @@ def get_appropriate_hourly_weather_instance_list(metric, postal_or_zip_code):
         print("UV Index: " + str(instance.uv_index))
         print("\n")
 
-    print(
-        " ------------------------------------------------------------------------------------------------------------ \n")
-
-    retrieve_info_class.retrieve_info.remove_incompatible_hourly_weather(hourly_weather_instance_list)
-
-    # For testing purposes
-    for instance in hourly_weather_instance_list:
-        if hourly_weather_class.hourly_weather.sunrise_time != None:
-            print("Sunrise: " + hourly_weather_class.hourly_weather.sunrise_time)
-
-        if hourly_weather_class.hourly_weather.sunset_time != None:
-            print("Sunset: " + hourly_weather_class.hourly_weather.sunset_time)
-
-        print("Time: " + hourly_weather_class.hourly_weather.time_tuple_to_string(*instance.time_tuple))
-        print("Real-Feel Temperature: " + str(instance.real_feel_temperature_tuple[0]) + str(
-            instance.real_feel_temperature_tuple[1]))
-        print("Precipitation Probability: " + str(instance.precipitation_probability))
-        print("UV Index: " + str(instance.uv_index))
-        print("\n")
-
-    print(
-        " ------------------------------------------------------------------------------------------------------------ \n")
-
     retrieve_info_class.retrieve_info.group_compatible_hourly_weather(hourly_weather_instance_list)
-
-    # For testing purposes:
-    for element in hourly_weather_instance_list:
-        if type(element) is hourly_weather_class.hourly_weather:
-            if element.twenty_four_hour_time.hour == 23:
-                next_hour = element.twenty_four_hour_time.hour.replace(hour=0)
-            else:
-                next_hour = element.twenty_four_hour_time.replace(hour=element.twenty_four_hour_time.hour + 1)
-
-            print("Time: " + hourly_weather_class.hourly_weather.time_tuple_to_string(
-                *element.time_tuple) + " to " + hourly_weather_class.hourly_weather.time_tuple_to_string(
-                hourly_weather_class.hourly_weather.convert_from_24_to_12_hour_time(next_hour)))
-        elif type(element) is tuple:
-            print("Time: " + hourly_weather_class.hourly_weather.time_tuple_to_string(
-                *element[0].time_tuple) + " to " + hourly_weather_class.hourly_weather.time_tuple_to_string(
-                *element[len(element) - 1].time_tuple))
 
     return hourly_weather_instance_list
 
@@ -106,6 +67,35 @@ def interface():
 
     root.mainloop()
 
+def extract_data(hourly_weather_instance_list):
+    # 2D array, each element is [time_interval, feels-like_temperature_tuple_list, uv_index_list]
+    data_list = []
+
+    for element in hourly_weather_instance_list:
+        if type(element) is hourly_weather_class.hourly_weather:
+            data_list[hourly_weather_instance_list.index(element)] = []
+
+            if element.twenty_four_hour_time.hour == 23:
+                next_hour = element.twenty_four_hour_time.hour.replace(hour=0)
+            else:
+                next_hour = element.twenty_four_hour_time.replace(hour=element.twenty_four_hour_time.hour + 1)
+
+            data_list[hourly_weather_instance_list.index(element)][0] = hourly_weather_class.hourly_weather.time_tuple_to_string(*element.time_tuple) + " - " + hourly_weather_class.hourly_weather.time_tuple_to_string(hourly_weather_class.hourly_weather.convert_from_24_to_12_hour_time(next_hour))
+            data_list[hourly_weather_instance_list.index(element)][1] = element.real_feel_temperature_tuple
+            data_list[hourly_weather_instance_list.index(element)][2] = element.uv_index
+        elif type(element) is tuple:
+            data_list[hourly_weather_instance_list.index(element)] = []
+            data_list[hourly_weather_instance_list.index(element)][1] = []
+            data_list[hourly_weather_instance_list.index(element)][2] = []
+
+            data_list[hourly_weather_instance_list.index(element)][0] = hourly_weather_class.hourly_weather.time_tuple_to_string(*element[0].time_tuple) + " - " + hourly_weather_class.hourly_weather.time_tuple_to_string(*element[len(element) - 1].time_tuple)
+
+            for item in element:
+                data_list[hourly_weather_instance_list.index(element)][1].append(item.real_feel_temperature_tuple)
+                data_list[hourly_weather_instance_list.index(element)][2].append(item.uv_index)
+
+    return data_list
+
 def main(option_menu_value, postal_or_zip_code, mainframe):
     if option_menu_value == "Imperial":
         metric = "false"
@@ -114,12 +104,11 @@ def main(option_menu_value, postal_or_zip_code, mainframe):
 
     hourly_weather_instance_list = get_appropriate_hourly_weather_instance_list(metric, postal_or_zip_code)
     header_column1_list = ["Best Time to Jog"]
-    header_column2_list = ["6:00am - 7:00am"]
-    header_column3_list = ["2:00pm - 3:00pm"]
     header_dictionary = dict()
-    header_dictionary["values"] = [header_column1_list, header_column2_list, header_column3_list]
+    header_dictionary["values"] = [header_column1_list]
+    header_dictionary["values"] = header_dictionary["values"] + extract_time_intervals(hourly_weather_instance_list)
 
-    cell_column1_list = ["Feels-like Temperature Range", "UV Index range"]
+    cell_column1_list = ["Feels-like Temperature Range", "UV Index Range"]
     cell_column2_list = [95, 85]
     cell_column3_list = [101, 102]
 
